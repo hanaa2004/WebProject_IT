c53349f288b23c2957c67dac44ca04ecc64e2b77
hanaa2004 <113058547+hanaa2004@users.noreply.github.com>
2023-03-30 12:34:48 +0200

    Add files via upload

---
+++ CSS/main.js/main.js
@@ -0,0 +1,12 @@
+let img = document.querySelector('.image');
+let container = document.querySelector('.container');
+function phones(phone){
+    img.src = phone;
+
+}
+function colors(color){
+    container.style.background = color;
+
+}
+
+
